12a411f3acfa96d5059a3d66be87cfbce0865c31
David Harper <david@obliquity.net>
2007-08-18 17:51:10 +0000

    Amended code to avoid clobbering original file when decrypting.
    git-svn-id: file:///disk2/repository/svn/trunk/aestool@529 32807195-9110-437f-9ebb-0d7c4c47859f

--- makeoutputfilename.c
+++ makeoutputfilename.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <strings.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include "aestool.h"
 
@@ -8,6 +11,8 @@ char * makeOutputFileName(char *infilename, int mode) {
   char *outfilename = NULL;
   int namelen;
   char *suffix;
+  struct stat stat_buf;
+  int rc;
 
   if (infilename == NULL || (mode != ENCRYPT && mode != DECRYPT))
     return NULL;
@@ -24,7 +29,15 @@ char * makeOutputFileName(char *infilename, int mode) {
     if (suffix != NULL && strcmp(suffix, ".aes") == 0) {
       outfilename = strdup(infilename);
       outfilename[namelen - 4] = '\0';
-    } else {
+
+      rc = stat(outfilename, &stat_buf);
+      if (rc == 0) {
+	free(outfilename);
+	outfilename = NULL;
+      }
+    } 
+
+    if (outfilename == NULL) {
       outfilename = malloc(namelen + 5);
       strcpy(outfilename, infilename);
       strcat(outfilename, ".raw");
